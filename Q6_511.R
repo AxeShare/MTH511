2798346d52e30c6307fd00cc70b563e72426846c
Akshay Bhatia <akshay.bhatia2007090@gmail.com>
2018-10-24 02:08:42 +0530

    Second Commit

--- Q6_511.R
+++ Q6_511.R
@@ -2,9 +2,16 @@ t <- 0
 avg <- 0
 avf <- 0
 i <- 0
+deg <- vector()
+deg[1] = 5
+deg[2] = 10
+deg[3] = 25
+deg[4] = 50
+rootr <- vector()
+d = 1
 
-for(g in c(6, 11, 26, 51)){
-  for(x in c("discrete" , "normal" , "cauchy" , "exponential")){
+for(x in c("discrete" , "normal" , "cauchy" , "exponential")){
+  for(g in c(6, 11, 26, 51)){
     while(TRUE){
       if(x == "discrete"){
         u <- runif(g)
@@ -36,6 +43,8 @@ for(g in c(6, 11, 26, 51)){
       avg <- t/i
   
       if(abs(avg - avf) < 1e-4 & avg != avf){
+        rootr[d] <- avg
+        d = d+1
         cat(sprintf("Degree of polynomial: %d,  Distribution is %s,   Average number of roots: %f,     Niterations: %d\n", g-1, x, avg, i))
         t <- 0
         avg <- 0
@@ -46,3 +55,11 @@ for(g in c(6, 11, 26, 51)){
     }
   }
 }
+
+plot(deg, rootr[1:4],"b", ylim = c(min(rootr),max(rootr)), col = "black", ylab = "Avg number of real roots", xlab = "degree of polynomial")
+lines(deg, rootr[5:8],"b", col = "red")
+lines(deg, rootr[9:12],"b", col = "green")
+lines(deg, rootr[13:16],"b", col = "blue")
+legend(5, 3.1, legend=c("Normal", "Discrete", "Cauchy", "Exponential"), col=c("red", "black", "green" , "blue"), lty = 1, cex=0.8, text.col = "black")
+
+
